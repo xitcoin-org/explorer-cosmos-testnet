61c517377fe4c3fa9f0dc3b246f3cd13e5f9ba43
Thomas <124535458+Thomas-Cabrit@users.noreply.github.com>
2026-08-30 19:30:46 +0200

    fix(explorer): standardize DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -247,7 +247,7 @@ const show_ad = computed(() => {
         <Sponsors v-if="showDiscord" />
         <div class="px-4 text-sm pt-2 text-gray-400 pb-2 uppercase">{{ $t('module.links') }}</div>
         <a
-          href="https://twitter.com/ping_pub"
+          href="https://twitter.com/xitcoin_org"
           target="_blank"
           class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
@@ -264,12 +264,12 @@ const show_ad = computed(() => {
           <div class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200">Discord</div>
         </a>
         <a
-          href="https://github.com/ping-pub/explorer/discussions"
+          href="https://xitcoin.gitbook.io/guide/"
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
           <Icon icon="mdi:frequently-asked-questions" class="text-xl mr-2" />
-          <div class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200">FAQ</div>
+          <div class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200">Guide</div>
         </a>
       </div>
     </div>
